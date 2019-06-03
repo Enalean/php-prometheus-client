edd489d6315d2224206417ec888cddc3af9317e2
Thomas Gerbet <thomas@gerbet.me>
2019-06-03 22:10:14 +0200

    Test that a metric can be rendered when no label is set

--- tests/unit/Prometheus/Renderer/RenderTextFormatTest.php
+++ tests/unit/Prometheus/Renderer/RenderTextFormatTest.php
@@ -36,6 +36,13 @@ final class RenderTextFormatTest extends TestCase
                     new Sample('test_some_metric_bucket', 0, ['le'], ['0.01']),
                 ]
             ),
+            new MetricFamilySamples(
+                'C',
+                'gauge',
+                'test C',
+                [],
+                [new Sample('test_some_metric_gauge', 0, [], [])]
+            ),
         ];
 
         $this->assertEquals(
@@ -46,6 +53,9 @@ test_some_metric_bucket{le="0.01"} 0
 # HELP B test B
 # TYPE B counter
 test_some_metric_counter{label1="value1",label2="value\\\\2",label3="value\n3"} 1
+# HELP C test C
+# TYPE C gauge
+test_some_metric_gauge 0
 ',
             $renderer->render($metrics)
         );
