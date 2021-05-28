59ccb0b699e46157ff92a14ddfd73911ac3266f0
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-05-28 11:46:43 +0200

    Drop PHP 7.3 support

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/84aa23742f6c72501f9cc209f29c438766f5352d.tar.gz") {} }:
 
 let
-  php = pkgs.php73.withExtensions ({ enabled, all }: with all; enabled ++ [ json redis apcu ]);
+  php = pkgs.php74.withExtensions ({ enabled, all }: with all; enabled ++ [ json redis apcu ]);
 in
 pkgs.mkShell {
   nativeBuildInputs = [
