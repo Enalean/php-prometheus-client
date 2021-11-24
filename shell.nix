10879e085166ce4695f3496292cb145998cdf4d1
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-11-24 10:30:07 +0100

    Update the provided Nix Shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,10 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/84aa23742f6c72501f9cc209f29c438766f5352d.tar.gz") {} }:
+{ pkgs ? import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/98747f27ecfee70c8c97b195cbb94df80a074dda.tar.gz";
+  sha256 = "04ss525ns5qqlggrdhvc6y4hqmshylda9yd0y99ddliyn15wmf27";
+}) {} }:
 
 let
-  php = pkgs.php74.withExtensions ({ enabled, all }: with all; enabled ++ [ json redis apcu ]);
+  php = pkgs.php80.withExtensions ({ enabled, all }: with all; enabled ++ [ redis apcu ]);
 in
 pkgs.mkShell {
   nativeBuildInputs = [
