b7b84baf07ec083d2990a052dceb7412b2caee20
Thomas Gerbet <thomas.gerbet@enalean.com>
2023-02-10 09:54:26 +0100

    Drop PHP 8.0 support

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/98747f27ecfee70c8c97b195cbb94df80a074dda.tar.gz";
-  sha256 = "04ss525ns5qqlggrdhvc6y4hqmshylda9yd0y99ddliyn15wmf27";
+  url = "https://github.com/NixOS/nixpkgs/archive/747927516efcb5e31ba03b7ff32f61f6d47e7d87.tar.gz";
+  sha256 = "1s4xabv59r99z8vd74w3r84kkxwqggqir3b0nh3ma04mni0m40gf";
 }) {} }:
 
 let
-  php = pkgs.php80.withExtensions ({ enabled, all }: with all; enabled ++ [ redis apcu ]);
+  php = pkgs.php81.withExtensions ({ enabled, all }: with all; enabled ++ [ redis apcu ]);
 in
 pkgs.mkShell {
   nativeBuildInputs = [
