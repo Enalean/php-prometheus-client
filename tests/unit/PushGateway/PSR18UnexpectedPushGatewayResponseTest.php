8357c1e1eed16d354441e7c5df989b7b57864979
Thomas Gerbet <thomas.gerbet@enalean.com>
2023-12-06 15:43:37 +0100

    Fix exception message when PSR18UnexpectedPushGatewayResponse is thrown
    The message was incorrect, it contained only some details but the
    context was missing.

--- tests/unit/PushGateway/PSR18UnexpectedPushGatewayResponseTest.php
+++ tests/unit/PushGateway/PSR18UnexpectedPushGatewayResponseTest.php
@@ -20,6 +20,7 @@ final class PSR18UnexpectedPushGatewayResponseTest extends TestCase
 
         $exception = PSR18UnexpectedPushGatewayResponse::invalidResponse($request, $response);
 
+        self::assertStringContainsString('Cannot connect PushGateway server to /: 500 Internal', $exception->getMessage());
         self::assertSame($request, $exception->getRequest());
         self::assertSame($response, $exception->getResponse());
         self::assertNull($exception->getPrevious());
@@ -29,11 +30,12 @@ final class PSR18UnexpectedPushGatewayResponseTest extends TestCase
     public function testRequestFailure(): void
     {
         $request         = Psr17FactoryDiscovery::findRequestFactory()->createRequest('PUT', '/');
-        $clientException = new class extends Exception implements ClientExceptionInterface {
+        $clientException = new class ('some reason') extends Exception implements ClientExceptionInterface {
         };
 
         $exception = PSR18UnexpectedPushGatewayResponse::requestFailure($request, $clientException);
 
+        self::assertStringContainsString('Cannot connect PushGateway server to /: some reason', $exception->getMessage());
         self::assertSame($request, $exception->getRequest());
         self::assertNull($exception->getResponse());
         self::assertSame($clientException, $exception->getPrevious());
