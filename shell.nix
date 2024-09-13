d0624dae50a3439afe1bf1d16753b5bb433332da
Thomas Gerbet <thomas.gerbet@enalean.com>
2024-09-13 14:51:16 +0200

    chore: request #39364 Update dev/build environment [W37 2024]
    Also moved to PHP 8.1 from PHP 8.2.

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/747927516efcb5e31ba03b7ff32f61f6d47e7d87.tar.gz";
-  sha256 = "1s4xabv59r99z8vd74w3r84kkxwqggqir3b0nh3ma04mni0m40gf";
+  url = "https://github.com/NixOS/nixpkgs/archive/280db3decab4cbeb22a4599bd472229ab74d25e1.tar.gz";
+  sha256 = "17n9wji64l7d16s8r100ypwlxkmwrypll4q3wkkfjswbilxkqjr6";
 }) {} }:
 
 let
-  php = pkgs.php81.withExtensions ({ enabled, all }: with all; enabled ++ [ redis apcu ]);
+  php = pkgs.php82.withExtensions ({ enabled, all }: with all; enabled ++ [ redis apcu ]);
 in
 pkgs.mkShell {
   nativeBuildInputs = [
